9a2a4b2ba1384dc19d4fc9f1f513bda5751156f6
AartiGandhi1 <137005505+AartiGandhi1@users.noreply.github.com>
2024-01-30 13:39:31 +0100

    Delete 05_wordblanks.js

--- 05_wordblanks.js
+++
@@ -1,9 +0,0 @@
-//combining missing words with return  function
-
-function wordBlanks (myNown, myAdjective, myVerb, myAdverb){
-
-    var result = "";
-    result= "My"+ myAdjective + myNown + myVerb + myAdverb;
-    return result;
-}
-console.log(wordBlanks(" dog", " big", " runs", " fast"));
